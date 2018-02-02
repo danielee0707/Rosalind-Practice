4ab69c49eb8005cbbc4ebe3143bda14ccd2fd66e
Chen Li <danielee0707@gmail.com>
2018-02-02 15:19:35 -0800

    update solution

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 def count_nucleotides(file_name):
     with open(file_name, 'r') as f:
-        sequence = f.read().rstrip()
+        sequence = ''.join(f.read().splitlines())
     my_dict = {'adenine': 0, 'cytosine': 0, 'guanine': 0, 'thymine': 0}
     for n in sequence:
         if n == 'A':
@@ -16,7 +16,7 @@ def count_nucleotides(file_name):
 
 def transcribe(file_name):
     with open(file_name, 'r') as f:
-        sequence = f.read().rstrip()
+        sequence = ''.join(f.read().splitlines())
     sequence = sequence.replace('T', 'U')
     return sequence
 
@@ -24,7 +24,7 @@ def transcribe(file_name):
 def complement(file_name):
     comp_seq = ''
     with open(file_name, 'r') as f:
-        sequence = f.read().rstrip()
+        sequence = ''.join(f.read().splitlines())
     sequence = sequence[::-1]
     for n in sequence:
         if n == 'A':
@@ -75,11 +75,11 @@ def gc_content(file_name):
 
 def count_snp(file_name):
     with open(file_name, 'r') as f:
-        sequence1 = f.readline().rstrip()
-        sequence2 = f.readline().rstrip()
+        seq1 = f.readline().rstrip()
+        seq2 = f.readline().rstrip()
     count = 0
-    for i in range(len(sequence1)):
-        if sequence1[i] != sequence2[i]:
+    for i in range(len(seq1)):
+        if seq1[i] != seq2[i]:
             count += 1
     return count
 
@@ -100,74 +100,81 @@ def mendel(k, m, n):
 
 def translate(file_name):
     codons = []
-    start_index = 0
+    start_index = -1
     peptide = []
     with open(file_name, 'r') as f:
-        sequence = f.read()
+        sequence = ''.join(f.read().splitlines())
     for i in range(len(sequence)):
         if sequence[i:(i + 3)] == 'AUG':
             start_index = i
             break
+    if start_index == -1:
+        return ''
     for i in range(start_index, len(sequence), 3):
         codons.append(sequence[i:(i + 3)])
-    if len(codons) == 0:
-        return ''
     if len(codons[-1]) != 3:
         codons.pop()
     for codon in codons:
-        if codon[0] == 'U':
-            if codon in ['UUU', 'UUC']:
-                peptide.append('F')
-            elif codon in ['UUA', 'UUG']:
-                peptide.append('L')
-            elif codon[1] == 'C':
-                peptide.append('S')
-            elif codon in ['UAU', 'UAC']:
-                peptide.append('Y')
-            elif codon in ['UAA', 'UAG', 'UGA']:
-                return peptide.join('')
-            elif codon in ['UGU', 'UGC']:
-                peptide.append('C')
-            elif codon == 'UGG':
-                peptide += 'W'
-        elif codon[0] == 'C':
-            if codon[1] == 'U':
-                peptide.append('L')
-            elif codon[1] == 'C':
-                peptide.append('P')
-            elif codon in ['CAU', 'CAC']:
-                peptide.append('H')
-            elif codon in ['CAA', 'CAG']:
-                peptide.append('Q')
-            elif codon[1] == 'G':
-                peptide.append('R')
-        elif codon[0] == 'A':
-            if codon in ['AUU', 'AUC', 'AUA']:
-                peptide.append('I')
-            elif codon == 'AUG':
-                peptide.append('M')
-            elif codon[1] == 'C':
-                peptide.append('T')
-            elif codon in ['AAU', 'AAC']:
-                peptide.append('N')
-            elif codon in ['AAA', 'AAG']:
-                peptide.append('K')
-            elif codon in ['AGU', 'AGC']:
-                peptide.append('S')
-            elif codon in ['AGA', 'AGG']:
-                peptide.append('R')
-        elif codon[0] == 'G':
-            if codon[1] == 'U':
-                peptide.append('V')
-            elif codon[1] == 'C':
-                peptide.append('A')
-            elif codon in ['GAU', 'GAC']:
-                peptide.append('D')
-            elif codon in ['GAA', 'GAG']:
-                peptide.append('E')
-            elif codon[1] == 'G':
-                peptide.append('G')
-    return peptide.join('')
+        aa = codon_table(codon)
+        if aa == '':
+            break
+        peptide.append(aa)
+    return ''.join(peptide)
+
+
+def codon_table(codon):
+    if codon[0] == 'U':
+        if codon in ['UUU', 'UUC']:
+            return 'F'
+        elif codon in ['UUA', 'UUG']:
+            return'L'
+        elif codon[1] == 'C':
+            return'S'
+        elif codon in ['UAU', 'UAC']:
+            return'Y'
+        elif codon in ['UAA', 'UAG', 'UGA']:
+            return ''
+        elif codon in ['UGU', 'UGC']:
+            return'C'
+        elif codon == 'UGG':
+            return 'W'
+    elif codon[0] == 'C':
+        if codon[1] == 'U':
+            return 'L'
+        elif codon[1] == 'C':
+            return 'P'
+        elif codon in ['CAU', 'CAC']:
+            return 'H'
+        elif codon in ['CAA', 'CAG']:
+            return 'Q'
+        elif codon[1] == 'G':
+            return 'R'
+    elif codon[0] == 'A':
+        if codon in ['AUU', 'AUC', 'AUA']:
+            return 'I'
+        elif codon == 'AUG':
+            return 'M'
+        elif codon[1] == 'C':
+            return 'T'
+        elif codon in ['AAU', 'AAC']:
+            return 'N'
+        elif codon in ['AAA', 'AAG']:
+            return 'K'
+        elif codon in ['AGU', 'AGC']:
+            return 'S'
+        elif codon in ['AGA', 'AGG']:
+            return 'R'
+    elif codon[0] == 'G':
+        if codon[1] == 'U':
+            return 'V'
+        elif codon[1] == 'C':
+            return 'A'
+        elif codon in ['GAU', 'GAC']:
+            return 'D'
+        elif codon in ['GAA', 'GAG']:
+            return 'E'
+        elif codon[1] == 'G':
+            return 'G'
 
 
 def find_motif(file_name):
@@ -300,12 +307,12 @@ def probability(k, n):
     return result
 
 
-def protein_motif(filename):
+def protein_motif(file_name):
     import urllib.request
     address = 'http://www.uniprot.org/uniprot/'
     result = []
     index = -1
-    with open(filename, 'r') as f:
+    with open(file_name, 'r') as f:
         ids = f.read().splitlines()
     for protein_id in ids:
         result.append(protein_id)
@@ -322,3 +329,89 @@ def protein_motif(filename):
                     else:
                         result[index] += ' ' + str(i + 1)
     return result
+
+
+def infer_rna(file_name):
+    aa_table = {'A': 4, 'R': 6, 'N': 2, 'D': 2, 'C': 2, 'Q': 2, 'E': 2, 'G': 4,
+                'H': 2, 'I': 3, 'L': 6, 'K': 2, 'M': 1, 'F': 2, 'P': 4, 'S': 6,
+                'T': 4, 'W': 1, 'Y': 2, 'V': 4}
+    result = 1
+    with open(file_name, 'r') as f:
+        protein = ''.join(f.read().splitlines())
+    for aa in protein:
+        result *= aa_table[aa]
+    return (result * 3) % 1000000
+
+
+def translate_orf(file_name):
+    result = []
+    with open(file_name, 'r') as f:
+        lines = f.read().splitlines()
+    sequence = ''.join(lines[1:])
+    comp_seq = []
+    rev_seq = sequence[::-1]
+    for n in rev_seq:
+        if n == 'A':
+            comp_seq.append('T')
+        elif n == 'C':
+            comp_seq.append('G')
+        elif n == 'G':
+            comp_seq.append('C')
+        elif n == 'T':
+            comp_seq.append('A')
+    comp_seq = ''.join(comp_seq)
+    orf = [sequence.replace('T', 'U'),
+           sequence[1:].replace('T', 'U'),
+           sequence[2:].replace('T', 'U'),
+           comp_seq.replace('T', 'U'),
+           comp_seq[1:].replace('T', 'U'),
+           comp_seq[2:].replace('T', 'U')]
+    for frame in orf:
+        start_indices = []
+        stop_indices = []
+        for i in range(0, len(frame), 3):
+            if frame[i:(i + 3)] == 'AUG':
+                start_indices.append(i)
+            if frame[i:(i + 3)] in ['UAA', 'UAG', 'UGA']:
+                stop_indices.append(i)
+        if not start_indices or not stop_indices:
+            continue
+        for start in start_indices:
+            if start > stop_indices[-1]:
+                continue
+            codons = []
+            peptide = []
+            for i in range(start, len(frame), 3):
+                codons.append(frame[i:(i + 3)])
+            if len(codons[-1]) != 3:
+                codons.pop()
+            for codon in codons:
+                aa = codon_table(codon)
+                if aa == '':
+                    break
+                peptide.append(aa)
+            result.append(''.join(peptide))
+    return list(set(result))
+
+
+def permutation(n):
+    from itertools import permutations
+    perm = permutations(range(1, n + 1))
+    p = list(perm)
+    for per in p:
+        print(' '.join(str(x) for x in per))
+    return len(p)
+
+
+def weight(file_name):
+    mass_tab = {'A': 71.03711, 'C': 103.00919, 'D': 115.02694, 'E': 129.04259,
+                'F': 147.06841, 'G': 57.02146, 'H': 137.05891, 'I': 113.08406,
+                'K': 128.09496, 'L': 113.08406, 'M': 131.04049, 'N': 114.04293,
+                'P': 97.05276, 'Q': 128.05858, 'R': 156.10111, 'S': 87.03203,
+                'T': 101.04768, 'V': 99.06841, 'W': 186.07931, 'Y': 163.06333}
+    w = 0
+    with open(file_name, 'r') as f:
+        protein = f.read().splitlines()
+        for a in protein[0]:
+            w += mass_tab[a]
+    return w
